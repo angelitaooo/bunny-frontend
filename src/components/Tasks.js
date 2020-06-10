b6ff3c4d94b1df3b326cfab5dbcb4b59e72bd611
Angela Ordonez <angelitaooo@me.com>
2020-06-09 23:33:08 -0500

    refactor: move Taks component into its own file

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,45 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { fetchTasks, updateTask } from '../data/api';
 import { useQuery, useMutation, queryCache } from 'react-query';
-
-const Task = ({ children, done, onBlur }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [newTaskName, setNewTaskName] = useState(children);
-  const striketrough = done ? 'line-through text-gray-400' : 'text-gray-700';
-
-  function handleOnBlur() {
-    setIsEditing(false);
-    onBlur(newTaskName);
-  }
-
-  return (
-    <div className="flex items-center">
-      {isEditing ? (
-        <div className="relative rounded-md shadow-sm">
-          <input
-            id="taskName"
-            className={`form-input block w-full sm:text-sm sm:leading-5`}
-            onBlur={handleOnBlur}
-            onChange={(e) => setNewTaskName(e.target.value)}
-            placeholder="Update your task - Should not be blank"
-            value={newTaskName}
-            autoFocus
-          />
-        </div>
-      ) : (
-        <div>
-          <span
-            className={`${striketrough}`}
-            onClick={() => setIsEditing(true)}
-          >
-            {children}
-          </span>
-        </div>
-      )}
-    </div>
-  );
-};
+import Task from './Task';
 
 const Tasks = ({ tasks }) => {
   const { userId } = useParams();
