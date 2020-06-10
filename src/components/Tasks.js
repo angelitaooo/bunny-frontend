7dff8f3181835e8e451f568046256fbb2cbf7b2c
Angela Ordonez <angelitaooo@me.com>
2020-06-09 23:28:56 -0500

    update tasks using checkboxes and edit task name

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { fetchTasks, updateTask } from '../data/api';
 import { useQuery, useMutation, queryCache } from 'react-query';
 
-const Task = ({ children, done, onClick }) => {
+const Task = ({ children, done, onBlur }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTaskName, setNewTaskName] = useState(children);
   const striketrough = done ? 'line-through text-gray-400' : 'text-gray-700';
+
+  function handleOnBlur() {
+    setIsEditing(false);
+    onBlur(newTaskName);
+  }
+
   return (
-    <li className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out border border-gray-200 cursor-pointer">
-      <button className="block min-w-full" onClick={onClick}>
-        <div className="px-4 py-4 sm:px-6">
-          <div className="flex items-center justify-between">
-            <div
-              className={`${striketrough} text-sm leading-5 font-medium truncate`}
-            >
-              {children}
-            </div>
-          </div>
+    <div className="flex items-center">
+      {isEditing ? (
+        <div className="relative rounded-md shadow-sm">
+          <input
+            id="taskName"
+            className={`form-input block w-full sm:text-sm sm:leading-5`}
+            onBlur={handleOnBlur}
+            onChange={(e) => setNewTaskName(e.target.value)}
+            placeholder="Update your task - Should not be blank"
+            value={newTaskName}
+            autoFocus
+          />
         </div>
-      </button>
-    </li>
+      ) : (
+        <div>
+          <span
+            className={`${striketrough}`}
+            onClick={() => setIsEditing(true)}
+          >
+            {children}
+          </span>
+        </div>
+      )}
+    </div>
   );
 };
 
@@ -36,10 +55,9 @@ const Tasks = ({ tasks }) => {
     return null;
   }
 
-  async function handleClick({ id, taskName, state }) {
-    const newState = state === 'done' ? 'todo' : 'done';
+  async function handleUpdateTask({ id, taskName, state }) {
     try {
-      await mutate({ taskName, userId, taskId: id, state: newState });
+      await mutate({ taskName, userId, taskId: id, state });
       history.push(`/users/${userId}`);
     } catch (error) {
       console.log(error.error);
@@ -49,13 +67,35 @@ const Tasks = ({ tasks }) => {
   return (
     <ul className="mt-4">
       {data.tasks.map((task) => (
-        <Task
-          key={task.id}
-          done={task.state === 'done'}
-          onClick={() => handleClick(task)}
-        >
-          {task.taskName}
-        </Task>
+        <li className="block flex justify-start">
+          <div className="flex items-center">
+            <input
+              id="remember_me"
+              type="checkbox"
+              className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-3"
+              onChange={() =>
+                handleUpdateTask({
+                  id: task.id,
+                  taskName: task.taskName,
+                  state: task.state === 'done' ? 'todo' : 'done',
+                })
+              }
+            />
+          </div>
+          <Task
+            key={task.id}
+            done={task.state === 'done'}
+            onBlur={(newTaskName) =>
+              handleUpdateTask({
+                id: task.id,
+                taskName: newTaskName,
+                state: task.state,
+              })
+            }
+          >
+            {task.taskName}
+          </Task>
+        </li>
       ))}
     </ul>
   );
